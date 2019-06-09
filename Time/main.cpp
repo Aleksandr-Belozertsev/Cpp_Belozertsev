0d97a2bd0b795b363c10e443fbe41873195c6ce7
Aleksandr-Belozertsev <aleksandr.belozertsev@phystech.edu>
2019-06-09 19:29:43 +0300

    Time

--- Time/main.cpp
+++ Time/main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
+
 #include "Time.h"
+#include "BasicTime.h"
 
 using namespace std;
+using namespace DateTime;
 
-/*cout << t1; // напечатает 01:23 03/04/2005
-cout << t2; // напечатает 02:34 04/05/2006
-switch (); // не обязательно именно так, но что-то похожее
-cout << t1; // напечатает 1:23 AM 04/03/2005
-cout << t1; // напечатает 2:34 AM 05/04/2006*/
+int main()
+{
+	try {
+		Time t1(01, 23, 03, 04, 2005, false);
+		Time t2(02, 34, 04, 05, 2006, false);
+		cout << t1 << endl;
+		cout << t2 << endl;
+		t1.setEuropeanFormat(false);
+		t2.setEuropeanFormat(false);
+		cout << t1 << endl;
+		cout << t2 << endl;
+		t1.setEuropeanFormat(true);
+		t2.setEuropeanFormat(true);
+		cout << t1 << endl;
+		cout << t2 << endl;
+		/*T + / -TI = T
+		T - T = TI
+		TI + / -TI = T*/
+
+		//TimeInterval ti1(0, 0, 10, 1, 1);
+		//TimeInterval ti2(0, 0, 1, 1, 2);
+		//Time tt = t1 + ti1;
+		//cout << tt << endl;
+		//Time tt1 = t2 - ti1;
+		//cout << tt1 << endl;
+		//TimeInterval t = t2 - t1;
+		//cout << t << endl;
+		//TimeInterval tt2 = ti2 - ti1;
+		//cout << tt2 << endl;
+		//TimeInterval tt3 = ti2 + ti1;
+		//cout << tt3 << endl;
+	}
+	catch (std::exception& e) { cout << e.what(); }
+	system("pause");
+}
